5fe88153dcfdfd6dfabc31b39fe3b293bd116c43
Ashok Tailor <ashok.tailor@gmail.com>
2020-02-18 14:35:09 +0000

    progress on creating an ec2 instance with terraform

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-    profile = "default"
-    region  = "eu-west-1"
+  profile = "default"
+  region  = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-    ami             = "ami-2757f631"
-    instance_type   = "t2.micro"
+  ami           = "ami-099a8245f5daa82bf"
+  instance_type = "t2.micro"
 }
